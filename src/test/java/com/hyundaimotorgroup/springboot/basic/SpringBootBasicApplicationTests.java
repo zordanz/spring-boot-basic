cff6e935a23b1431e49e118c491e0a024286a111
jordan.jung <jordan@hyunday.com>
2025-10-09 19:55:57 +0900

    Initial commit

---
+++ src/test/java/com/hyundaimotorgroup/springboot/basic/SpringBootBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyundaimotorgroup.springboot.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
