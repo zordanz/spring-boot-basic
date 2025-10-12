9ccf806bb9fe567c94ce4be2cb9c7cd7475dad87
정명주 책임매니저 <jordan@hyundai.com>
2025-10-12 22:48:57 +0900

    dev-time services(redis cache)

--- src/main/java/com/hyundaimotorgroup/springboot/basic/typesafeconfig/AppProperties.java
+++ src/main/java/com/hyundaimotorgroup/springboot/basic/typesafeconfig/AppProperties.java
@@ -7,11 +7,9 @@ import org.springframework.validation.annotation.Validated;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "app")
-// FIXME: 2. @Validated, @Valid 애노테이션으로 유효성 체크
 @Validated
 public record AppProperties(@Valid PersonProperties owner,
                             @Valid PersonProperties manager,
-                            // FIXME: 3. staffs 추가
                             @Valid Map<String, PersonProperties> staffs
 ) {
 }
--- src/main/java/com/hyundaimotorgroup/springboot/basic/typesafeconfig/PersonProperties.java
+++ src/main/java/com/hyundaimotorgroup/springboot/basic/typesafeconfig/PersonProperties.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotEmpty;
 
 import java.io.Serializable;
 
-// FIXME: 5. 직렬화 설정. @see JdkSerializationRedisSerializer
-public record PersonProperties(@NotEmpty String firstName, @NotEmpty String lastName) implements Serializable {
+public record PersonProperties(@NotEmpty String firstName, @NotEmpty String lastName) {
 
 }
