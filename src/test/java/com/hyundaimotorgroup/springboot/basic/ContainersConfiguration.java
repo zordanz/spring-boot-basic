77cc80fd148384bd231dca25fe3fccb69f56e9d5
정명주 책임매니저 <jordan@hyundai.com>
2025-10-12 23:36:10 +0900

    devtime2

--- src/test/java/com/hyundaimotorgroup/springboot/basic/ContainersConfiguration.java
+++ src/test/java/com/hyundaimotorgroup/springboot/basic/ContainersConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistrar;
 import org.testcontainers.utility.DockerImageName;
 
 // FIXME: 2. Redis Testcontainers 설정
