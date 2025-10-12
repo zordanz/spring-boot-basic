56f79c5332f40e012720dda9fa8672f179b8961d
정명주 책임매니저 <jordan@hyundai.com>
2025-10-12 18:40:24 +0900

    typesafe-config

--- src/main/java/com/hyundaimotorgroup/springboot/basic/typesafeconfig/PersonProperties.java
+++ src/main/java/com/hyundaimotorgroup/springboot/basic/typesafeconfig/PersonProperties.java
@@ -4,6 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 // TODO: 1. @ConfigurationProperties 애노테이션
 // TODO: 2. Constructor Binding 으로 완성하기.
-public record PersonProperties(String firstName, String lastName) {
+public record PersonProperties() {
 
 }
