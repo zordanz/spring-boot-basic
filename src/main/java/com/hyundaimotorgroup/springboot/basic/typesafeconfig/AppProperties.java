ff618835b263b98d953b6e10814afa5c3cf2c09b
정명주 책임매니저 <jordan@hyundai.com>
2025-10-20 17:47:28 +0900

    fix AppProperties

--- src/main/java/com/hyundaimotorgroup/springboot/basic/typesafeconfig/AppProperties.java
+++ src/main/java/com/hyundaimotorgroup/springboot/basic/typesafeconfig/AppProperties.java
@@ -2,14 +2,15 @@ package com.hyundaimotorgroup.springboot.basic.typesafeconfig;
 
 import jakarta.validation.Valid;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "app")
 @Validated
-public record AppProperties(@Valid PersonProperties owner,
-                            @Valid PersonProperties manager,
-                            @Valid Map<String, PersonProperties> staffs
+public record AppProperties(@Valid @NestedConfigurationProperty PersonProperties owner,
+                            @Valid @NestedConfigurationProperty PersonProperties manager,
+                            Map<String, @Valid PersonProperties> staffs
 ) {
 }
