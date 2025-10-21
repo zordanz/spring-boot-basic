5b1d7fbba8f4d8b1fc027718e91b095105d551ee
정명주 책임매니저 <jordan@hyundai.com>
2025-10-21 15:33:18 +0900

    junit extension

---
+++ src/test/java/com/hyundaimotorgroup/springboot/basic/junit/extension/RandomDemoTest.java
@@ -0,0 +1,50 @@
+package com.hyundaimotorgroup.springboot.basic.junit.extension;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class RandomDemoTest {
+    private static final Logger log = LoggerFactory.getLogger(RandomDemoTest.class);
+    // including @BeforeAll or @AfterEach lifecycle methods.
+    @Random
+    private static Integer randomNumber0;
+
+    static {
+        log.info("randomNumber0: {} in static", randomNumber0);
+    }
+
+    // Use randomNumber1 field in test methods and @BeforeEach or @AfterEach lifecycle methods.
+    @Random
+    private int randomNumber1;
+
+    RandomDemoTest(@Random int randomNumber2) {
+        // Use randomNumber2 in constructor.
+        log.info("randomNumber0: {} in Constructor", randomNumber0);
+        log.info("randomNumber1: {} in Constructor", randomNumber1);
+        log.info("randomNumber2: {} in Constructor", randomNumber2);
+    }
+
+    @BeforeEach
+    void beforeEach(@Random int randomNumber3) {
+        // Use randomNumber3 in @BeforeEach method.
+        log.info("randomNumber0: {} in beforeEach", randomNumber0);
+        log.info("randomNumber1: {} in beforeEach", randomNumber1);
+        log.info("randomNumber3: {} in beforeEach", randomNumber3);
+    }
+
+    @Test
+    void test1(@Random int randomNumber4) {
+        log.info("randomNumber0: {} in test1", randomNumber0);
+        log.info("randomNumber1: {} in test1", randomNumber1);
+        log.info("randomNumber4: {} in test1", randomNumber4);
+    }
+
+    @Test
+    void test2(@Random int randomNumber4) {
+        log.info("randomNumber0: {} in test2", randomNumber0);
+        log.info("randomNumber1: {} in test2", randomNumber1);
+        log.info("randomNumber4: {} in test2", randomNumber4);
+    }
+}
---
+++ src/test/java/com/hyundaimotorgroup/springboot/basic/junit/extension/RandomNumberExtension.java
@@ -0,0 +1,77 @@
+package com.hyundaimotorgroup.springboot.basic.junit.extension;
+
+import static org.junit.platform.commons.support.AnnotationSupport.findAnnotatedFields;
+
+import java.lang.reflect.Field;
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolver;
+import org.junit.jupiter.api.extension.TestInstancePostProcessor;
+import org.junit.platform.commons.support.ModifierSupport;
+
+class RandomNumberExtension
+    implements BeforeAllCallback, TestInstancePostProcessor, ParameterResolver {
+
+    private final java.util.Random random = new java.util.Random(System.nanoTime());
+
+    /**
+     * Inject a random integer into static fields that are annotated with
+     * {@code @Random} and can be assigned an integer value.
+     */
+    @Override
+    public void beforeAll(ExtensionContext context) {
+        Class<?> testClass = context.getRequiredTestClass();
+        injectFields(testClass, null, ModifierSupport::isStatic);
+    }
+
+    /**
+     * Inject a random integer into non-static fields that are annotated with
+     * {@code @Random} and can be assigned an integer value.
+     */
+    @Override
+    public void postProcessTestInstance(Object testInstance, ExtensionContext context) {
+        Class<?> testClass = context.getRequiredTestClass();
+        injectFields(testClass, testInstance, ModifierSupport::isNotStatic);
+    }
+
+    /**
+     * Determine if the parameter is annotated with {@code @Random} and can be
+     * assigned an integer value.
+     */
+    @Override
+    public boolean supportsParameter(ParameterContext pc, ExtensionContext ec) {
+        return pc.isAnnotated(Random.class) && isInteger(pc.getParameter().getType());
+    }
+
+    /**
+     * Resolve a random integer.
+     */
+    @Override
+    public Integer resolveParameter(ParameterContext pc, ExtensionContext ec) {
+        return this.random.nextInt();
+    }
+
+    private void injectFields(Class<?> testClass, Object testInstance, Predicate<Field> predicate) {
+
+        predicate = predicate.and(field -> isInteger(field.getType()));
+        findAnnotatedFields(testClass, Random.class, predicate)
+            .forEach(field -> {
+                try {
+                    field.setAccessible(true);
+                    field.set(testInstance, this.random.nextInt());
+                }
+                catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
+    }
+
+    private static boolean isInteger(Class<?> type) {
+        return type == Integer.class || type == int.class;
+    }
+
+}
+
